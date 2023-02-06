ba678a668a25292b4e1becf6c30048405bf7a595
erikwibowo <erikwibo@gmail.com>
2023-02-07 02:48:34 +0700

    remove dupplicate code and add import class middleware:Localization

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 
 class Localization
 {
@@ -16,12 +18,9 @@ class Localization
      */
     public function handle(Request $request, Closure $next)
     {
-        if(\Session::has('locale'))
-        {
-            \App::setlocale(\Session::get('locale'));
+        if (Session::has('locale')) {
+            App::setlocale(Session::get('locale'));
         }
-        return $next($request);
-     
         return $next($request);
     }
 }
