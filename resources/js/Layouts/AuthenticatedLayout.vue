26a5e6620d4b4e0d00b7c1f3e57f922fa36b3a07
inhs <harrysudana@gmail.com>
2023-02-15 19:45:04 +0800

    Change NavBar to Navbar

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-import NavBarVue from '@/Components/NavBar.vue';
+import NavbarVue from '@/Components/Navbar.vue';
 import SideBarVue from "@/Components/SideBar.vue";
 import Toast from '@/Components/Toast.vue';
 import Footer from '@/Components/Footer.vue';
@@ -14,7 +14,7 @@ const emit = defineEmits(['close', 'open']);
         <SideBarVue :open="sidebarOpened" @close="sidebarOpened = false" />
         <div class="pl-0 lg:pl-64 w-full min-h-screen block bg-gray-100 dark:bg-gray-900">
             <Toast :flash="$page.props.flash" />
-            <NavBarVue :open="sidebarOpened" @open="sidebarOpened = true" />
+            <NavbarVue :open="sidebarOpened" @open="sidebarOpened = true" />
             <!-- Page Content -->
             <main class="max-w-7xl mx-auto sm:px-6 lg:px-8 pb-10 text-gray-900 dark:text-gray-100 text-sm">
                 <slot />
