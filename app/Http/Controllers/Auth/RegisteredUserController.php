6d2b99557a226ad0dea2d8af3a84f539d079f135
erikwibowo <erikwibo@gmail.com>
2023-03-01 16:21:57 +0700

    asign role default: operator for register user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,6 +42,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $user->assignRole('operator');
 
         event(new Registered($user));
 
