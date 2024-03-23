8648264943f71e959613fe057087fb49fd593c2e
harrysudana <harrysudana@gmail.com>
2024-03-23 15:46:22 +0800

    Upgrade to Laravel 11
    Upgrade spatie laravel-permission to v6
    
    https://spatie.be/docs/laravel-permission/v6/upgrading#content-upgrading-from-v5-to-v6
    
    https://github.com/spatie/laravel-permission/discussions/2576#discussioncomment-8417545

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,8 +66,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
-        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
-        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
     ];
 }
