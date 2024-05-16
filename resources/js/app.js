58a1be66167ea2774780faf41e04ac419c6a03da
erikwibowo <erikwibo@gmail.com>
2024-05-16 15:51:58 +0700

    build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,24 @@
-import './bootstrap';
-import '../css/app.css';
-import 'floating-vue/dist/style.css'
+import "./bootstrap";
+import "../css/app.css";
+import "floating-vue/dist/style.css";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-import FloatingVue from 'floating-vue'
-import { usePage } from '@inertiajs/vue3';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import FloatingVue from "floating-vue";
+import { usePage } from "@inertiajs/vue3";
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
@@ -31,12 +36,12 @@ createInertiaApp({
                     },
                     lang: function () {
                         return usePage().props.language.original;
-                    }
+                    },
                 },
             })
             .mount(el);
     },
     progress: {
-        color: '#0284c7',
+        color: "#0284c7",
     },
 });
